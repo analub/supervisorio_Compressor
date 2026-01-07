5d7788b56492844123ec287dffb089cb680b86d9
Mariana Luciano Saar <mariana.saar@estudante.ufjf.br>
2026-01-07 16:33:15 -0300

    adicionei o arquivo popups.py

---
+++ popups.py
@@ -0,0 +1,20 @@
+from kivy.uix.popup import Popup
+
+
+class ModbusPopup(Popup):
+    """
+    Popup da configuração do MODBUS
+    """
+    pass
+
+class ScanPopup(Popup):
+    """
+    Popup da configuração do tempo de varredura
+    """
+    def __init__(self, scantime, **kwargs):
+        """
+        Construtor da classe ScanPopup
+        """
+        super().__init__(**kwargs)
+        self.ids.txt_st.text = str(scantime)
+    pass
