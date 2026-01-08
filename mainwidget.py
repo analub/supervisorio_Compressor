420d3428d2de71002513f8b82f3dc09642adad11
Ana Luisa Basilio de Castro <bc.analu04@gmail.com>
2026-01-08 16:02:19 -0300

    feat: alterando para usar popups

--- mainwidget.py
+++ mainwidget.py
@@ -1,7 +1,16 @@
 from kivy.uix.boxlayout import BoxLayout
+from popups import ModbusPopup, ScanPopup
 
 class MainWidget(BoxLayout):
     """
         Widget principal do aplicativo
     """
-    pass
+    def __init__(self, **kwargs):
+        """
+        Construtor do widget principal
+        """
+        super().__init__()
+        self._scan_time = kwargs.get('scan_time')
+        self.modbusPopup = ModbusPopup()
+        self.scanPopup = ScanPopup(scantime=self._scan_time)
+
