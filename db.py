821ec5260d5beaf290ca565fe4308846b205cb1d
carvalhomanu <emanuelle.carvalho@estudante.ufjf.br>
2026-01-08 14:53:08 -0300

    Objetos do sqlalchemy para manipulação do banco de dados

---
+++ db.py
@@ -0,0 +1,14 @@
+"""
+Objetos do sqlalchemy core para realização da conexão e operação do Banco de Dados
+"""
+
+from sqlalchemy import create_engine, engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+DB_CONNECTION = 'sqlite://data\data.db?check_same_thread=False'
+engine = create_engine(DB_CONNECTION, echo=False)
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+
