d51a89bbc8a0a1d0bab84552e93c4ff42c69638c
Mariana Luciano Saar <mariana.saar@estudante.ufjf.br>
2026-01-08 16:18:19 -0300

    refactor: move arquivos da interface para a subpasta correta

--- supervisorio_Compressor/popups.py
+++ supervisorio_Compressor/popups.py
@@ -1,20 +1,18 @@
 from kivy.uix.popup import Popup
 
-
 class ModbusPopup(Popup):
     """
-    Popup da configuração do MODBUS
+        Popup para configuração do protocolo Modbus
     """
     pass
 
 class ScanPopup(Popup):
     """
-    Popup da configuração do tempo de varredura
+        Popup para exibir o progresso da varredura 
     """
-    def __init__(self, scantime, **kwargs):
+    def __init__(self, scantime,**kwargs): #receber varios argumentos sem precisar especificar cada um
         """
-        Construtor da classe ScanPopup
+        Construtor da classe ScanPopup"
         """
         super().__init__(**kwargs)
         self.ids.txt_st.text = str(scantime)
-    pass
