ca505828473c97f40f92cf6d7ba3adfc44cfef0c
Ana Luisa Basilio de Castro <bc.analu04@gmail.com>
2026-01-12 09:33:06 -0300

    feat: ajuste botoes e funcionalidades

--- mainwidget.py
+++ mainwidget.py
@@ -1,16 +1,17 @@
 from kivy.uix.boxlayout import BoxLayout
-from popups import ModbusPopup, ScanPopup
+from popups import ModbusPopup, ScanPopup, ComandoPopup
 
 class MainWidget(BoxLayout):
     """
         Widget principal do aplicativo
     """
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs):    #criar/colocar os popups aqui
         """
         Construtor do widget principal
         """
         super().__init__()
         self._scan_time = kwargs.get('scan_time')
-        self.modbusPopup = ModbusPopup()
-        self.scanPopup = ScanPopup(scantime=self._scan_time)
+        self._modbusPopup = ModbusPopup()
+        self._scanPopup = ScanPopup(scantime=self._scan_time)
+        self._comandoPopup = ComandoPopup()
 
