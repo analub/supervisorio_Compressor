c2a6341960d80f061fd7f7e4ca2d8105d433f1ef
carvalhomanu <emanuelle.carvalho@estudante.ufjf.br>
2026-01-19 23:17:46 -0300

    correção de escrita

--- db.py
+++ db.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
-DB_CONNECTION = 'sqlite://data\data.db?check_same_thread=False'
+DB_CONNECTION = 'sqlite:///data\data.db?check_same_thread=False'
 engine = create_engine(DB_CONNECTION, echo=False)
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
